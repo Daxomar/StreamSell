abdd60d3c1dcc82d59f6d0778a322283444df6ca
Daxomar <152349184+Daxomar@users.noreply.github.com>
2026-08-10 13:31:09 +0000

    Pushing with landing page v3

--- app/auth/Verifyandreset/layout.jsx
+++ app/auth/Verifyandreset/layout.jsx
@@ -1,24 +1,24 @@
-import Link from "next/link"
-import { useUser,UserProvider } from "@/app/contexts/UserContext"
-import { AuthProvider,useAuth } from "@/app/contexts/AuthContext"
-import RoleGate from "@/app/contexts/RoleGate"
+// import Link from "next/link"
+// import { useUser,UserProvider } from "@/app/contexts/UserContext"
+// import { AuthProvider,useAuth } from "@/app/contexts/AuthContext"
+// import RoleGate from "@/app/contexts/RoleGate"
 
 
 
 
-export default function VerificationAndResetLayout({ children }) {
-  return (
-    <div className="">
-      <div className="">
+// export default function VerificationAndResetLayout({ children }) {
+//   return (
+//     <div className="">
+//       <div className="">
 
-        <AuthProvider>
-        <UserProvider>
-          <RoleGate allowedRoles={["user","admin"]}>
-            {children}
-          </RoleGate>
-        </UserProvider>
-        </AuthProvider>
-      </div>
-    </div>
-  )
-}
+//         <AuthProvider>
+//         <UserProvider>
+//           <RoleGate allowedRoles={["user","admin"]}>
+//             {children}
+//           </RoleGate>
+//         </UserProvider>
+//         </AuthProvider>
+//       </div>
+//     </div>
+//   )
+// }
